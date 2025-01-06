feac7570209a3beca1c56d2efb423d5fef40bc86
Shahriyor Adhamov <nasirovairoda41@gmail.com>
2025-01-06 21:13:12 +0500

    feat: add repeater  task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (!options.separator) options.separator = "+";
+  if (!options.additionSeparator) options.additionSeparator = "|";
+  if (typeof options.addition === "undefined") {
+    options.addition = "";
+    options.additionSeparator = "";
+  }
+
+  let chunk = String(str) + Array.from({ length: options.additionRepeatTimes || 1 }, () => String(options.addition)).join(options.additionSeparator);
+  return Array.from({ length: options.repeatTimes || 1 }, () => chunk).join(options.separator);
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
